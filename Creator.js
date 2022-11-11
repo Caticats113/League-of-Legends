60a3c16ffc29ecc64ac83e8749eacf76dbf17567
Anne Larregui <anne.solarte99@gmail.com>
2022-11-11 18:41:25 -0500

    homeCreatorReady

--- Creator.js
+++ Creator.js
@@ -4,15 +4,16 @@ class Creator{
         this.img= img;
         this.description = description;
     }
-    render(){
-        let html = ""
-        html =`<img src="./ImagenesHome/image 63.png" alt="Integrante abierto" id="memojiGrande">
-        <h3 id="textoNeiriña">Andrés Neira</h3>
-        <p id="textoNeiriña">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel facilis ab ullam, voluptate cupiditate ex veritatis natus vero, fuga ut optio adipisci enim distinctio iusto, architecto aperiam doloremque eum molestias cum officiis! Ipsam vitae odio amet ullam dolores officia minus quaerat ea cum, totam reiciendis, numquam excepturi eligendi aspernatur id!</p>`
+    render(container){
+        let html = "";
+        html =`<div class="memojiMain">
+                <img src="${this.img}" alt="Integrante abierto" id="memojiGrande">
+                <h3 id="texto">${this.name}</h3>
+                <p id="texto">${this.description}</p>
+                </div>`
 
-        let creator = document.createElement(`div`)
-        creator.classList.add("memojiMain")
-        creator.innerHTML = html;
-        container.appendChild(creator);
+        container.innerHTML = html;
     }
+
+
 }
--- home..js
+++
@@ -1,2 +0,0 @@
-const creator = document.getElementById('creator')
-
